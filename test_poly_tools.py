74d3bef07e3d5396b3f6a04ccbcf79d87af3f65e
Julian Hamre <julian.hamre5@gmail.com>
2022-06-18 17:35:28 +0200

    add test_get_coef method

--- test_poly_tools.py
+++ test_poly_tools.py
@@ -7,6 +7,7 @@ Created on Sat Jun 18 16:46:54 2022
 """
 
 import unittest
+import numpy as np
 import poly_tools as pt
 
 class testPoly_tools(unittest.TestCase):
@@ -15,6 +16,14 @@ class testPoly_tools(unittest.TestCase):
         reversed_list = [1, 2, 3, 4, 5]
         func_reversed_list = pt.reverse(forward_list)
         self.assertEqual(reversed_list, func_reversed_list)
+
+    def test_get_coef(self):
+        coef = [3, 2, 4]
+        array_coef = np.array(coef)
+        poly = pt.polynomial(array_coef)
+        get_coef = poly.get_coef()
+        self.assertEqual(coef, get_coef)
+
 
 if __name__ == "__main__":
     unittest.main()
