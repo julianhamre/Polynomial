315929bd2ff0e8594280befd48c78df16dc28008
Julian Hamre <julian.hamre5@gmail.com>
2022-06-19 22:35:55 +0200

    change 1_and_2 names to all

--- test_poly_tools.py
+++ test_poly_tools.py
@@ -21,50 +21,50 @@ def round_numbers(numbers, decimals):
 class testPoly(unittest.TestCase):
     coef_1 = [3, 2, 4]
     coef_2 = [5, -2, 3.2942, -5, -9.52, -2]
-    coef_1_and_2 = [coef_1, coef_2]
-    ln = len(coef_1_and_2)
+    all_coef = [coef_1, coef_2]
+    ln = len(all_coef)
    
     def test_reverse(self):
         reversed_1 = [4, 2, 3]
         reversed_2 = [-2, -9.52, -5, 3.2942, -2, 5]
-        rev_1_and_2 = [reversed_1, reversed_2]
+        all_rev = [reversed_1, reversed_2]
         for i in range(0, self.ln):
-            func_rev = pt.reverse(self.coef_1_and_2[i])
-            self.assertEqual(rev_1_and_2[i], func_rev)
+            func_rev = pt.reverse(self.all_coef[i])
+            self.assertEqual(all_rev[i], func_rev)
 
     def array_poly(self, i):
-         return pt.polynomial(np.array(self.coef_1_and_2[i]))
+         return pt.polynomial(np.array(self.all_coef[i]))
 
     def test_get_coef(self):
         for i in range(0, self.ln):
             poly = self.array_poly(i)
             gc = poly.get_coef()
-            self.assertEqual(self.coef_1_and_2[i], gc)
+            self.assertEqual(self.all_coef[i], gc)
 
     def test_coef_as_list(self):
         for i in range(0, self.ln):
             poly = self.array_poly(i)
             poly.coef_as_list()
-            self.assertEqual(poly.coef, self.coef_1_and_2[i])        
+            self.assertEqual(poly.coef, self.all_coef[i])        
     
     def test_evaluate(self):
         values = [0, 3, -2.54, 50]
         coef_1_eval = [4, 37, 18.2748, 7604]
         coef_2_eval = [-2, 1066.3834, -675.9197, 1550398797]
-        eval_1_and_2 = [coef_1_eval, coef_2_eval]
+        all_eval = [coef_1_eval, coef_2_eval]
         for i in range(0, self.ln):
-            poly = pt.polynomial(self.coef_1_and_2[i])
+            poly = pt.polynomial(self.all_coef[i])
             meth_eval = poly.evaluate_values(values)
             meth_eval = round_numbers(meth_eval, 4)
-            self.assertEqual(meth_eval, round_numbers(eval_1_and_2[i], 4))
+            self.assertEqual(meth_eval, round_numbers(all_eval[i], 4))
     
     def test_differentiate(self):
         dif_1 = [6, 2]
         dif_2 = [25, -8, 9.8826, -10, -9.52]
-        dif_1_and_2 = [dif_1, dif_2]
+        all_dif = [dif_1, dif_2]
         for i in range(0, self.ln):
-            poly = pt.polynomial(self.coef_1_and_2[i])
-            self.assertEqual(poly.differentiate().get_coef(), dif_1_and_2[i])
+            poly = pt.polynomial(self.all_coef[i])
+            self.assertEqual(poly.differentiate().get_coef(), all_dif[i])
 
 
 if __name__ == "__main__":
