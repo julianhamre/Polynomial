cf6e15ac1de40ab245bbba25c017e5678db0af43
Julian Hamre <julian.hamre5@gmail.com>
2022-06-19 11:10:23 +0200

    add test_get_coef and test_coef_as_list method, strengthen tests and refactor

--- test_poly_tools.py
+++ test_poly_tools.py
@@ -10,19 +10,34 @@ import unittest
 import numpy as np
 import poly_tools as pt
 
-class testPoly_tools(unittest.TestCase):
+class testPoly(unittest.TestCase):
+    coef_1 = [3, 2, 4]
+    coef_2 = [5, -2, 3.2942, -5, -9.52, -2]
+    coef_1_and_2 = [coef_1, coef_2]
+    ln = len(coef_1_and_2)
+    
     def test_reverse(self):
-        forward_list = [5, 4, 3, 2, 1]
-        reversed_list = [1, 2, 3, 4, 5]
-        func_reversed_list = pt.reverse(forward_list)
-        self.assertEqual(reversed_list, func_reversed_list)
+        reversed_1 = [4, 2, 3]
+        reversed_2 = [-2, -9.52, -5, 3.2942, -2, 5]
+        rev_1_and_2 = [reversed_1, reversed_2]
+        for i in range(0, self.ln):
+            func_rev = pt.reverse(self.coef_1_and_2[i])
+            self.assertEqual(rev_1_and_2[i], func_rev)
+
+    def array_poly(self, i):
+         return pt.polynomial(np.array(self.coef_1_and_2[i]))
 
     def test_get_coef(self):
-        coef = [3, 2, 4]
-        array_coef = np.array(coef)
-        poly = pt.polynomial(array_coef)
-        get_coef = poly.get_coef()
-        self.assertEqual(coef, get_coef)
+        for i in range(0, self.ln):
+            poly = self.array_poly(i)
+            gc = poly.get_coef()
+            self.assertEqual(self.coef_1_and_2[i], gc)
+
+    def test_coef_as_list(self):
+        for i in range(0, self.ln):
+            poly = self.array_poly(i)
+            poly.coef_as_list()
+            self.assertEqual(poly.coef, self.coef_1_and_2[i])
 
 
 if __name__ == "__main__":
