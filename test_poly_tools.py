16c72f652816c6da086a93914d491b818ee8f323
Julian Hamre <julian.hamre5@gmail.com>
2022-06-21 16:48:38 +0200

    remove define_coef method, change variable name p to poly

--- test_poly_tools.py
+++ test_poly_tools.py
@@ -25,14 +25,11 @@ class testPoly(unittest.TestCase):
  
     def create_array_poly(self):
         return pt.polynomial(np.array(self.coef))
-    
-    def define_coef(self):
-        self.coef = self.test_coef
         
     def setUp(self):
         self.coef = [5, -2, 3.2942, 0, -9.52, -2]
-        self.p = self.create_poly()
-        self.array_p = self.create_array_poly()
+        self.poly = self.create_poly()
+        self.array_poly = self.create_array_poly()
 
     def test_reverse(self):
         rev = [-2, -9.52, 0, 3.2942, -2, 5]
@@ -40,24 +37,24 @@ class testPoly(unittest.TestCase):
         self.assertEqual(rev, func_rev)
 
     def test_get_coef(self):
-        get_c = self.array_p.get_coef()
+        get_c = self.array_poly.get_coef()
         self.assertEqual(self.coef, get_c)
 
     def test_coef_as_list(self):
-        self.array_p.coef_as_list()
-        self.assertEqual(self.p.coef, self.coef)        
+        self.array_poly.coef_as_list()
+        self.assertEqual(self.poly.coef, self.coef)        
 
     def test_evaluate(self):
         values = [0, -3, -7.54, 56]
         evaluated = [-2, -1439.3834, -129656.8601, 2734567867.1072]
-        method_evaluated = self.p.evaluate_values(values)
+        method_evaluated = self.poly.evaluate_values(values)
         round_method_evaluated = round_numbers(method_evaluated, 4)
         self.assertEqual(round_method_evaluated, evaluated)
     
     def test_differentiate(self):
         dif = [25, -8, 9.8826, 0, -9.52]
-        p_dif = self.p.differentiate()
-        self.assertEqual(p_dif.get_coef(), dif)
+        poly_dif = self.poly.differentiate()
+        self.assertEqual(poly_dif.get_coef(), dif)
 
     def test_add_constant(self):
         constants = [0, 1, 104, -4, -3.6]
@@ -65,9 +62,9 @@ class testPoly(unittest.TestCase):
         method_results = []
         for i in constants:
             self.coef = [5, -2, 3.2942, 0, -9.52, -2]
-            p = self.create_poly()
-            p.add_constant(i)
-            method_results.append(p.coef[-1])
+            refresh_poly = self.create_poly()
+            refresh_poly.add_constant(i)
+            method_results.append(refresh_poly.coef[-1])
         self.assertEqual(results, method_results) 
 
 
